e6c1696f92b3518a6eb0e5cf47f9861647876a44
Devream <devream24@gmail.com>
2025-01-27 10:57:08 +0100

    Work on Padlock

--- Assets/Scripts/CombinationPadlock.cs
+++ Assets/Scripts/CombinationPadlock.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CombinationPadlock : MonoBehaviour
 {
@@ -9,29 +10,113 @@ public class CombinationPadlock : MonoBehaviour
 
     [SerializeField] private GameObject puzzleScreen;
     [SerializeField] private Camera focusCamera;
+
     [SerializeField] private int[] secretCode;
+
+    [SerializeField] private Button[] buttonsAdd;
+    [SerializeField] private Button[] buttonsSubstract;
+
+    [SerializeField] private GameObject[] numbers;
+
+    private int[] inputCode;
     private Animator animator;
     private bool isCorrect = false;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+
+        inputCode = new int[secretCode.Length];
+
+        for(int i = 0; i < secretCode.Length; i++)
+        {
+            inputCode[i] = 0;
+        }
+
+        for (int i = 0; i < buttonsAdd.Length; i++)
+        {
+            int position = i;
+            buttonsAdd[i].onClick.AddListener(() => ChangeNumber(position, true));
+        }
+
+        for (int i = 0; i < buttonsSubstract.Length; i++)
+        {
+            int position = i;
+            buttonsSubstract[i].onClick.AddListener(() => ChangeNumber(position, false));
+        }
+
     }
 
-    public void CheckPassword(string password)
+    public void ChangeNumber(int position, bool addNum)
     {
-        // Verificar que la longitud de la contraseña sea la misma que el código secreto
-        if (password.Length != secretCode.Length)
+        // Get the current rotation of the object
+        Transform targetTransform = numbers[position].transform;
+        Quaternion currentRotation = targetTransform.rotation;
+
+        // Calculate the desired rotation
+
+        float rotationAmount = 0;
+
+        if (addNum)
         {
-            isCorrect = false;
-            return;
+            rotationAmount = 36f;
+            inputCode[position] = (inputCode[position] + 1) % 10;
         }
-        isCorrect = SearchForIncorrectValue(secretCode, password);
+        else
+        {
+            rotationAmount = -36f;
+            inputCode[position] = (inputCode[position] - 1 + 10) % 10;
+        }
+
+
+        Quaternion desiredRotation = currentRotation * Quaternion.Euler(rotationAmount, 0, 0);
+
+        // Start the rotation coroutine
+        StartCoroutine(RotateOverTime(targetTransform, currentRotation, desiredRotation, 0.1f)); // 0.5f is the duration
     }
+
+    private IEnumerator RotateOverTime(Transform target, Quaternion startRotation, Quaternion endRotation, float duration)
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            // Lerp between the start and end rotations based on elapsed time
+            target.rotation = Quaternion.Lerp(startRotation, endRotation, elapsedTime / duration);
+
+            elapsedTime += Time.deltaTime;
+            yield return null; // Wait for the next frame
+        }
+
+        // Ensure the final rotation is set
+        target.rotation = endRotation;
+    }
+
+
+    public void CheckPassword()
+    {
+
+        for (int i = 0; i < inputCode.Length; i++)
+        {
+            Debug.Log(inputCode[i]);
+        }
+
+
+            /*
+            // Verificar que la longitud de la contraseña sea la misma que el código secreto
+            if (password.Length != secretCode.Length)
+            {
+                isCorrect = false;
+                return;
+            }
+            isCorrect = SearchForIncorrectValue(secretCode, password);
+            */
+        }
 
     private bool SearchForIncorrectValue(int[] correctPassword, string inputPassword)
     {
         int i = 0;
+
         while (i < correctPassword.Length)
         {
             // Intentamos convertir cada caracter de la contraseña en un número
@@ -41,17 +126,9 @@ public class CombinationPadlock : MonoBehaviour
             }
             i++;
         }
+
         return true;
     }
-
-
-
-
-
-
-
-
-
 
     // Metodo llamado a través de la animacion
     public void ExitPadlock()
@@ -79,7 +156,6 @@ public class CombinationPadlock : MonoBehaviour
         Cursor.visible = true;  // Hacer visible el cursor
 
         yield return new WaitForSeconds(1f);
-        animator.SetTrigger("Open");
     }
 
     private void OnTriggerStay(Collider other)

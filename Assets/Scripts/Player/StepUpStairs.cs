f836eef224765282ed3a2f11a60071c44e6f2db4
sandroAntonucci <146188422+sandroAntonucci@users.noreply.github.com>
2025-02-04 16:34:18 +0100

    Stairs Update

--- Assets/Scripts/Player/StepUpStairs.cs
+++ Assets/Scripts/Player/StepUpStairs.cs
@@ -1,34 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class StepUpStairs : MonoBehaviour
+public class PlayerGroundCheck : MonoBehaviour
 {
-    public float playerHeight = 2.0f;
-    public float stairHeight = 0.5f;
-    public float detectionDistance = 1.0f;
-    public float climbSpeed = 0.1f;
-    public LayerMask stairsLayer;
+    public float groundRayDistance = 1.2f; // Distance for ground check
+    public float extraGravity = 20f; // Additional gravity to keep player grounded
+    public LayerMask groundLayer; // Set this to detect ground layers
 
-    private Transform playerTransform;
-    private Vector3 heightOffset;
+    private Rigidbody rb;
 
     void Start()
     {
-        playerTransform = GetComponent<Transform>();
-        heightOffset = new Vector3(0, playerHeight, 0);
+        rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         RaycastHit hit;
-        Vector3 rayOrigin = playerTransform.position + heightOffset;
-        Vector3 rayDirection = playerTransform.forward;
+        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, out hit, groundRayDistance, groundLayer);
 
-        if (Physics.Raycast(rayOrigin, rayDirection, out hit, detectionDistance, stairsLayer))
+        Debug.Log(isGrounded);
+
+        if (!isGrounded)
         {
-            Vector3 climbPosition = new Vector3(playerTransform.position.x, playerTransform.position.y + stairHeight, playerTransform.position.z);
-            playerTransform.position = Vector3.Lerp(playerTransform.position, climbPosition + playerTransform.forward, climbSpeed);
+            rb.AddForce(Vector3.down * extraGravity, ForceMode.Acceleration);
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        // Visualizing the ray in the editor
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundRayDistance);
     }
 }

0b3c66bbaf51da246bd755b8b8379ebdf36ed573
sandroAntonucci <146188422+sandroAntonucci@users.noreply.github.com>
2025-02-12 17:44:51 +0100

    Closet Update

---
+++ Assets/Scripts/ClosetHide.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosetHide : MonoBehaviour
+{
+
+    [SerializeField] private GameObject interactionText;
+
+    [SerializeField] private MoveMenuCamera cameraInside;
+    [SerializeField] private MoveMenuCamera cameraOutside;
+
+    private bool playerInRange = false;
+
+    private bool playerInsideCloset = false;
+
+    private bool playerOpeningCloset = false;   
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerObj")
+        {
+            interactionText.SetActive(true);
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.tag == "PlayerObj")
+        {
+            interactionText.SetActive(false);
+            playerInRange = false;
+        }
+
+    }
+
+    private IEnumerator EnterCloset()
+    {
+
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().rb.velocity = Vector3.zero;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().enabled = false;
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<PlayerCam>().enabled = false;
+        GameObject.FindGameObjectWithTag("PlayerInterface").GetComponent<Canvas>().enabled = false;
+
+        StartCoroutine(cameraInside.CameraMovement());
+
+        playerOpeningCloset = true;
+
+        cameraOutside.finalCameraPos = cameraInside.initialCameraPos;
+        cameraOutside.finalCameraRotation = cameraInside.initialCameraRotation;
+
+        interactionText.SetActive(false);
+        anim.Play("Closet_Open");
+
+        yield return new WaitForSeconds(cameraInside.duration);
+
+        playerOpeningCloset = false;
+        playerInsideCloset = true;
+
+
+
+    }
+
+    private IEnumerator ExitCloset()
+    {
+
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().enabled = true;
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<PlayerCam>().enabled = true;
+        GameObject.FindGameObjectWithTag("PlayerInterface").GetComponent<Canvas>().enabled = true;
+
+        playerOpeningCloset = true;
+
+        StartCoroutine(cameraOutside.CameraMovement());
+
+        anim.Play("Closet_Open");
+
+        yield return new WaitForSeconds(cameraInside.duration);
+
+
+        playerOpeningCloset = false;
+        playerInsideCloset = false;
+        interactionText.SetActive(true);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange && !playerOpeningCloset)
+        {
+
+
+            if (!playerInsideCloset) StartCoroutine(EnterCloset());
+            
+            else StartCoroutine(ExitCloset());
+
+        }
+    }
+
+}
